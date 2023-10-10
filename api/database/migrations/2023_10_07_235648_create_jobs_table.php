1322685ba7a169cf87e45e8baa79ecc163f7b677
sasaki-cooboo <sasaki@cooboo.co.jp>
2023-10-11 08:49:13 +0900

    update:dbにコメント追加

--- api/database/migrations/2023_10_07_235648_create_jobs_table.php
+++ api/database/migrations/2023_10_07_235648_create_jobs_table.php
@@ -15,16 +15,16 @@ return new class extends Migration
     {
         Schema::create('jobs', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('area_id')->constrained();
-            $table->foreignId('language_id')->constrained();
-            $table->foreignId('skill_id')->constrained();
-            $table->foreignId('enginner_type_id')->constrained();
-            $table->foreignId('feature_id')->constrained();
-            $table->string('title');
-            $table->integer('cost');
-            $table->string('description', 1000);
-            $table->json('required_skills');
-            $table->string('message');
+            $table->foreignId('area_id')->constrained()->comment('地域ID');
+            $table->foreignId('language_id')->constrained()->comment('言語ID');
+            $table->foreignId('skill_id')->constrained()->comment('スキルID');
+            $table->foreignId('enginner_type_id')->constrained()->comment('エンジニア種別ID');
+            $table->foreignId('feature_id')->constrained()->comment('特徴ID');
+            $table->string('title')->comment('タイトル');
+            $table->integer('cost')->comment('単価');
+            $table->string('description', 1000)->comment('案件の詳細');
+            $table->json('required_skills')->comment('必要なスキル');
+            $table->string('message')->comment('メッセージ');
             $table->timestamps();
         });
     }
