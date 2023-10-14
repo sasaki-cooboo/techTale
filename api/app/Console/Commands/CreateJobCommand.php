c0c91f56b9b974515ca6d96dae50926a77887565
sasaki-cooboo <sasaki@cooboo.co.jp>
2023-10-14 09:57:37 +0900

    update:必要なスキルをランダムに修正

--- api/app/Console/Commands/CreateJobCommand.php
+++ api/app/Console/Commands/CreateJobCommand.php
@@ -34,7 +34,30 @@ class CreateJobCommand extends Command
     private $messages = ["リモートでの作業を想定しておりますので、リモート案件をお探しの方にもおすすめです。", "関係部署と連携しながら作業していただくため、積極的にコミュニケーションを取っていける方にマッチします。", "長期プロジェクトを想定しておりますので、腰を据えての参画を希望されている方にもおすすめです。", "関係部署と連携しながら作業していただきますので、コミュニケーションを取りながらの作業が得意な方にマッチします。", "風通しが良く、積極的に新しい技術や提案を取り入れる環境ですのでモダンな環境でスキルアップしたい方にもおすすめです。", "フルスタックエンジニアとしてのご経験が豊富な方、チームでコミュニケーションを取りながらの作業が得意な方によりマッチします。", "将来的に新サービスの展開も視野にいれており、コミュニケーションが良好な現場ですので切磋琢磨しながらスキルアップしていきたい方におすすめです。", "新規機能開発の設計 / 実装を担当いただきます。TypeScriptなどの開発経験が豊富な方にマッチします。", "某大手事業会社での案件です。風通しのいい環境で働きたい方におすすめです。", "リーダー経験を積んでいきたい方にもおすすめの環境です。基本的にリモートでの作業を想定しております。"];
 
     private $required_skills = [
-        "Webアプリケーションサービス開発経験（2年以上）", "Gitでのチーム開発経験", "テックリード経験",
+        "Webアプリケーションサービス開発経験（2年以上）",
+        "Gitでのチーム開発経験",
+        "テックリード経験",
+        "AWSでのアプリケーション開発経験",
+        "要件定義～テスト開発実務経験（３年以上）",
+        "システム運用実績（1年以上）",
+        "Webアプリケーションの設計 / 運用経験",
+        "SQLでのデータ抽出経験",
+        "非エンジニアの要望をヒアリングし、仕様案を作成した経験",
+        "チームでの開発経験",
+        "新規サービスの開発からリリースまでの経験",
+        "基本設計以降の一貫した経験",
+        "パフォーマンスを考慮した設計経験",
+        "単体テストフレームワークでの開発実績",
+        "フレームワークでの開発実績",
+        "言語問わず、2つ以上の言語での開発実務経験（3年以上）",
+        "運用改善の実務経験",
+        "システムメンテナンスの実務経験",
+        "アジャイルでの開発実務経験",
+        "MySQLなどのDB使用経験",
+        "AWS環境でのサーバ構築経験",
+        "API開発経験",
+        "Webサービスの開発運用経験（1年以上）",
+        "要件定義 / 基本設計 / 詳細設計経験（3年以上）"
     ];
 
     private const TIMES = 100;
@@ -47,6 +70,8 @@ class CreateJobCommand extends Command
     public function handle(BatchProcessingService $batchProcessingService)
     {
         $faker = Faker::create();
+
+        $ramdomRequiredSkills = shuffle($this->required_skills);
 
         for ($i = 0; $i < self::TIMES; $i++) {
 
@@ -64,7 +89,8 @@ class CreateJobCommand extends Command
                 'title' => $title,
                 'cost' => $cost,
                 'description' => $this->descriptions[rand(0, count($this->descriptions) - 1)],
-                'required_skills' => json_encode($this->required_skills),
+                // 必要なスキルはランダムに3つ抽出する
+                'required_skills' => json_encode([$ramdomRequiredSkills[0], $ramdomRequiredSkills[1], $ramdomRequiredSkills[2]]),
                 'message' => $this->messages[rand(0, count($this->messages) - 1)],
                 'created_at' => $now,
                 'updated_at' => $now,
