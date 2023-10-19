bbb15a9e36324efe0b651413c6ee70f60510130d
sasaki-cooboo <sasaki@cooboo.co.jp>
2023-10-20 07:40:27 +0900

    add:sortフロント作成

--- front/src/atoms/atoms.ts
+++ front/src/atoms/atoms.ts
@@ -14,3 +14,6 @@ export const initialJobCondition = {
 };
 
 export const jobConditionAtom = atom<JobConditionType>(initialJobCondition);
+
+export const MENU_LIST = ["関連度順", "新着順", "高単価順"] as const;
+export const jobSortAtom = atom<(typeof MENU_LIST)[number]>(MENU_LIST[0]);
--- front/src/features/jobs/SortMenu.tsx
+++ front/src/features/jobs/SortMenu.tsx
@@ -3,14 +3,27 @@ import Button from "@mui/material/Button";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { useAtom, useSetAtom } from "jotai";
+import {
+  MENU_LIST,
+  jobAtom,
+  jobConditionAtom,
+  jobSortAtom,
+  loadingAtom,
+} from "@/atoms/atoms";
+import { useRouter } from "next/router";
+import { convertObjectToQueryString } from "@/libs/convertQuery";
+import { JobListResponse } from "./job.type";
+import fetch from "@/libs/fetch";
 
 export default function SortMenu() {
-  const MENU_LIST = ["関連度順", "新着順", "高単価順"] as const;
+  const [sortOption, setSortOption] = useAtom(jobSortAtom);
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
-  const [selectedOption, setSelectedOption] = React.useState<string>(
-    MENU_LIST[0]
-  );
+  const setData = useSetAtom(jobAtom);
+  const setLoading = useSetAtom(loadingAtom);
+  const [condition, setCondition] = useAtom(jobConditionAtom);
+  const router = useRouter();
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
@@ -20,9 +33,26 @@ export default function SortMenu() {
     setAnchorEl(null);
   };
 
-  const handleMenuItemClick = (option: string) => {
-    setSelectedOption(option); // 選択した選択肢にテキストを更新
+  const handleMenuItemClick = async (option: (typeof MENU_LIST)[number]) => {
+    setSortOption(option); // 選択した選択肢にテキストを更新
     setAnchorEl(null); // メニューを閉じる
+    try {
+      setLoading(true);
+      const queryString = convertObjectToQueryString(condition);
+      const sortQuery =
+        option === "高単価順" ? "&sort=cost" : "新着順" ? "&sort=latest" : null;
+      const { data } = await fetch.get<JobListResponse>(
+        `/api/v1/jobs${queryString}${sortQuery}`
+      );
+      setData(data);
+      router.push(`/job/search/${queryString}${sortQuery}`, undefined, {
+        shallow: true,
+      });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -36,7 +66,7 @@ export default function SortMenu() {
         endIcon={<KeyboardArrowDownIcon />}
         variant="text"
       >
-        {selectedOption}
+        {sortOption}
       </Button>
       <Menu
         id="sort-menu"
