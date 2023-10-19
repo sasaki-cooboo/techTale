c5e0674cb8acfa24479230bff31d787a6d5226e1
sasaki-cooboo <sasaki@cooboo.co.jp>
2023-10-20 06:59:30 +0900

    update:fontの修正

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,8 +1,5 @@
 import Head from "next/head";
-import { Inter } from "next/font/google";
 import { GetServerSideProps } from "next";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
